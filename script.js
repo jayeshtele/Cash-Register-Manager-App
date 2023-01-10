5f6794ccdbdfed599a72cf641fd228f48e696937
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-01-10 21:37:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const billAmount = document.querySelector("#billAmount");
 const cashGiven = document.querySelector("#cashGiven");
 const check = document.querySelector(".btn-success");
 
-cashGiven.addEventListener("onkeyup",()=>{
-    console.log("clicked")
+check.addEventListener("click", () => {
+
+    if (!billAmount.value == "" && !isNaN(billAmount.value) && billAmount.value > 0) {
+
+    } else {
+        alert("Enter the amount in proper positive number")
+    }
+
+
 })
