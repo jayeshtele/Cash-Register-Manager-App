3684f3f106379665c682b80455f25d6b3339bfbf
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-01-10 17:44:01 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const billAmount = document.querySelector("#billAmount");
+const cashGiven = document.querySelector("#cashGiven");
+const check = document.querySelector(".btn-success");
+
+cashGiven.addEventListener("onkeyup",()=>{
+    console.log("clicked")
+})
